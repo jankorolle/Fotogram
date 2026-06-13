fd4603332d81cd2bcbf759539c06274377949065
Janko Rolle <jankorolle@gmail.com>
2026-06-13 19:48:51 +0200

    Basic HTML and Images added

---
+++ Functions.js
@@ -0,0 +1,13 @@
+let myImages = [
+    "0872285883cee475f9533508e645f19ef0939ae8.png",
+    "3c416b08502bf2ed10a304bdce5c1c782b99c40d.jpg",
+    "4ac67c63bfc2b6a80cca4919df91a1e498ec4c07.jpg",
+    "714707b1ea80f159dacba47280dc1091bbebb9c7.jpg",
+    "872f2a95ab48c88b865e4f11d2e061a802987c88.jpg",
+    "8bc8dae8f655d7cac8d770f66d76f62367f5b5df.jpg",
+    "bbd3479cb0c36ecd872f4526275e11a893e50107.jpg",
+    "cc1d7f8870c4b98e849a8e36c6c397e0f71486f9.jpg",
+    "dc4b11f7f36deb9409236c10ebcd6c849b00f447.jpg",
+    "f1ba9135a20ea8343ad3d5732c6f8a541ec455b5.jpg",
+    "f2b056a08d5edba809ca216fa6aa66a4bb612ea8.jpg",
+]
